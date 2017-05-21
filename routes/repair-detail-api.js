f02e307ad6c80ddf6fb30cf0eeda89ff07cc4823
dsoraluz <d.soraluz@gmail.com>
2017-05-21 02:32:55 -0400

    nodemailer and email sent on request

--- routes/repair-detail-api.js
+++ routes/repair-detail-api.js
@@ -7,6 +7,8 @@ const Device = require('../models/device-model');
 const Customer = require('../models/customer-model');
 const Location = require('../models/location-model');
 
+const nodemailer = require('nodemailer');
+
 
 //Route that returns all repair details from API
 repairDetailsApi.get('/repair-details',(req,res,next)=>{
@@ -92,6 +94,63 @@ repairDetailsApi.post('/repair-details',(req,res,next)=>{
       message: 'new device added!',
       id: theRepair._id
     });
+
+    const companyEmail = process.env.GMAIL_USERNAME;
+
+    let toFirstName = req.body.firstName;
+    let phone = req.body.phone;
+    let email = req.body.email;
+    let county = req.body.county;
+    let area = req.body.area;
+    let device = req.body.device;
+    let model = req.body.model;
+    let color = req.body.color;
+    let issue = req.body.repairType;
+    let cost = req.body.repairCost;
+    let date = req.body.requestedDate;
+    let time = req.body.requestedTime;
+
+    // Create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.GMAIL_USERNAME, //User Email address
+          pass: process.env.GMAIL_PASSWORD
+        }
+      });
+
+      // Setup email data with unicode symbols
+      let mailOptions = {
+        // Sender address
+        from: `Pristine Wireless <pristine.wireless@gmail.com>`,
+        // List of receivers
+        to: `${companyEmail}, ${email}`,
+        // Subject Line
+        subject: `The Next Step To Repair Your Device`,
+        // Plain text body
+        html: `<h4> Device: ${device} ${model}, ${color} </h4><br/><br/>
+        <h4> Issue: ${issue} </h4><br/><br/>
+        <h4> cost: ${cost} </h4><br/><br/>
+        <h4> Date and Time: ${date}, ${time} </h4><br/><br/>
+        <h4> Name: ${toFirstName} </h4><br/><br/>
+        <h4> County: ${county} </h4><br/><br/>
+        <h4> Area: ${area} </h4><br/><br/>
+        <h4> Email: ${email} </h4><br/><br/>
+        <h4> Phone: ${phone} </h4><br/><br/>`
+      };
+
+      // Send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info)=>{
+        if (error){
+          res.send(500);
+          console.log(error);
+          return;
+        }else {
+          console.log('Message %s sent: %s', info.messageId, info.response);
+          transporter.close();
+          // res.redirect('/');
+        }
+      });
   });
 });
 
